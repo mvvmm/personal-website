f083df001964ec199d801415eed2002cf1354956
mvm <root.mvm@gmail.com>
2025-11-10 20:55:03 -0600

    profile photo

--- components/photo.tsx
+++ components/photo.tsx
@@ -1,25 +1,27 @@
 import Image from "next/image";
 
-const IMAGE_OUTPUT_SIZE = 200;
-const ORIGINAL_IMAGE_WIDTH = 633;
-const ORIGINAL_IMAGE_HEIGHT = 610;
+const IMAGE_OUT_WIDTH = 200;
+const ORIGINAL_IMAGE_WIDTH = 600;
+const ORIGINAL_IMAGE_HEIGHT = 600;
+
+const IMAGE_OUTPUT_SCALE = IMAGE_OUT_WIDTH / ORIGINAL_IMAGE_WIDTH;
 
 export default function Photo() {
 	return (
 		<div
-			className="rounded-full bg-neutral-300"
+			className="rounded-full bg-slate-400 dark:bg-slate-600"
 			style={{
-				width: `${IMAGE_OUTPUT_SIZE}px`,
-				height: `${IMAGE_OUTPUT_SIZE}px`,
+				width: `${ORIGINAL_IMAGE_WIDTH * IMAGE_OUTPUT_SCALE}px`,
+				height: `${ORIGINAL_IMAGE_WIDTH * IMAGE_OUTPUT_SCALE}px`,
 			}}
 		>
 			<Image
 				className="rounded-full"
 				style={{
-					width: `${IMAGE_OUTPUT_SIZE}px`,
-					height: `${IMAGE_OUTPUT_SIZE}px`,
+					width: `${ORIGINAL_IMAGE_WIDTH * IMAGE_OUTPUT_SCALE}px`,
+					height: `${ORIGINAL_IMAGE_WIDTH * IMAGE_OUTPUT_SCALE}px`,
 				}}
-				src="/vance-morrison.webp"
+				src="/vance-morrison.png"
 				alt="Vance Morrison"
 				width={ORIGINAL_IMAGE_WIDTH}
 				height={ORIGINAL_IMAGE_HEIGHT}
