ed417c4841473058dbe6c0421070ff61e7fdc003
mvm <root.mvm@gmail.com>
2025-11-10 18:01:51 -0600

    add theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { ModeToggle } from "@/components/theme-toggle";
-
 export default function Home() {
-	return (
-		<div>
-			<h1>Hello world</h1>
-			<ModeToggle />
-		</div>
-	);
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  );
 }
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,39 +1,27 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+export function ThemeToggle() {
+  const { resolvedTheme, setTheme } = useTheme();
 
-export function ModeToggle() {
-	const { setTheme } = useTheme();
+  const toggleTheme = () => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+  };
 
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size="icon">
-					<Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-					<Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-					<span className="sr-only">Toggle theme</span>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuItem onClick={() => setTheme("light")}>
-					Light
-				</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("dark")}>
-					Dark
-				</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("system")}>
-					System
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
-	);
+  return (
+    <Button
+      aria-label="Toggle theme"
+      className="rounded-full"
+      variant="ghost"
+      size="icon-lg"
+      onClick={toggleTheme}
+    >
+      <Sun className="scale-100 dark:scale-0" />
+      <Moon className="absolute scale-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
 }
